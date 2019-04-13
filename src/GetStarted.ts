bfdd95107117ff1d7ab8f3294f052b5e856dc38d
ALiangLiang <me@aliangliang.top>
2019-04-13 19:55:35 +0800

    refactor(getstarted)

--- src/GetStarted.ts
+++ src/GetStarted.ts
@@ -2,10 +2,9 @@ import { Abstract } from './Abstract'
 
 export class GetStarted extends Abstract {
   constructor (payload: String) {
-    const
-      constructure = {
-        get_started: { payload }
-      }
+    const constructure = {
+      get_started: { payload }
+    }
 
     super(constructure)
   }
