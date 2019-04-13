fb2c0daad1c2e8cdb6fca23c1526f70027fcadc7
ALiangLiang <me@aliangliang.top>
2019-04-13 18:21:54 +0800

    test(buttonelementshare): add test

---
+++ test/integration/ButtonElementShare.test.ts
@@ -0,0 +1,28 @@
+import { expect } from 'chai'
+
+import { ButtonElementShare, TemplateGeneric } from '../../src'
+
+describe('ButtonElementShare', function () {
+  it('should work', function () {
+    let json = (new ButtonElementShare({
+      shareContents: new TemplateGeneric([
+        new TemplateGeneric.Element('Shared template')
+      ])
+    })).toJSON()
+
+    expect(json).to.eql({
+      type: 'element_share',
+      share_contents: {
+        attachment: {
+          type: 'template',
+          payload: {
+            template_type: 'generic',
+            elements: [{
+              title: 'Shared template'
+            }]
+          }
+        }
+      }
+    })
+  })
+})
